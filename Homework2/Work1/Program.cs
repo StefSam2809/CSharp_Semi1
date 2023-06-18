1bc77fbd287c1b1134e927f9c6293511b7d2a329
Stefan Samoylov <stefan28091996@gmail.com>
2023-06-18 14:34:00 +0500

    первая задача решена

--- Homework2/Work1/Program.cs
+++ Homework2/Work1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿System.Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int numbera = number / 10;
+int numberb = numbera % 10;
+ 
+System.Console.WriteLine(numberb);
