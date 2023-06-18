131ae287d401027f5dda8d78be1bbf15b46cec7d
Stefan Samoylov <stefan28091996@gmail.com>
2023-06-18 15:28:40 +0500

    вторая задача решена

--- Homework2/Work2/Program.cs
+++ Homework2/Work2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number / 100 == 0)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+int x = number;
+int z = 1;
+while (x > 0)
+{
+    x = x / 10;
+    z = z * 10;
+} z = z / 1000;
+
+int numbera = number / z;
+int numberb = numbera % 10;
+
+System.Console.WriteLine(numberb);
