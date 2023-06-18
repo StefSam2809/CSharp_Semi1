248fa6853b3b6d0f5db4b58cbc48ecb139d9998b
Stefan Samoylov <stefan28091996@gmail.com>
2023-06-18 15:43:34 +0500

    третья задача решена

--- Homework2/Work3/Program.cs
+++ Homework2/Work3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number/8 > 0)
+{
+    System.Console.WriteLine("Некорректный ввод");
+} else if (number / 5 > 0)  
+{
+    System.Console.WriteLine("Выходной день");
+} else System.Console.WriteLine("Рабочий день");
