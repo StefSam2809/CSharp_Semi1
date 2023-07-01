6d00f0301ce312396873bcddf33990a399529b47
Stefan Samoylov <stefan28091996@gmail.com>
2023-07-01 12:52:11 +0500

    третья задача третьего занятия решена

--- Homework3/Work3/Program.cs
+++ Homework3/Work3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while(i <= num)
+{
+    System.Console.WriteLine(Math.Pow(i , 3));
+    i++;
+}
