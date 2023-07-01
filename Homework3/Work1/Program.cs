3e41331b69dea7880fa993924b8f02bb62fff23d
Stefan Samoylov <stefan28091996@gmail.com>
2023-07-01 12:19:05 +0500

    первая задача третьего занятия решена

--- Homework3/Work1/Program.cs
+++ Homework3/Work1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+string? str = a.ToString();
+
+if(a > 10000 && a < 99999)
+{
+    if(str[0] == str[4] && str[1] == str[3])
+    {
+         System.Console.WriteLine("Число палиндром"); 
+    } else System.Console.WriteLine("Число не палиндром");
+    
+} else System.Console.WriteLine("Некорректный ввод");
