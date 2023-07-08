18648e5bf8b0935431bedf6655a9754ef88e7234
Stefan Samoylov <stefan28091996@gmail.com>
2023-07-08 14:27:58 +0500

    Третья решена

--- Homework5/Work3/Program.cs
+++ Homework5/Work3/Program.cs
@@ -1,7 +1,25 @@
-﻿int[] array = new int[4];
+﻿double[] array = new double[4];
 Random rand = new Random();
 for (int i=0; i < array.Length; i++)
 {
-    array[i] = rand.Next(100, 1000);
+    array[i] = Math.Round(rand.NextDouble()*100, 2);
 }
+double max = array[0];
+double min = array[0];
+for (int i=0; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+}
+for (int i=0; i < array.Length; i++)
+{
+    if (min > array[i])
+    {
+        min = array[i];
+    }
+}
+double number = max - min;
 System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine(number);
