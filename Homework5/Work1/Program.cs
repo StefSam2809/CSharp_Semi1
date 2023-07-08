7542db0cfb9206469590c911e1bf9208e6b32821
Stefan Samoylov <stefan28091996@gmail.com>
2023-07-08 13:23:31 +0500

    Первая решена

--- Homework5/Work1/Program.cs
+++ Homework5/Work1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int[] array = new int[4];
+int x = 0;
+Random rand = new Random();
+for (int i=0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+    if (array[i]%2 == 0)
+    {
+        x++;
+    }
+}
+System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine(x);
--- Homework5/Work3/Program.cs
+++ Homework5/Work3/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿int[] array = new int[4];
+Random rand = new Random();
+for (int i=0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+}
+System.Console.WriteLine(string.Join(", ", array));
