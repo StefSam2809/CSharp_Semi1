35c34a9daed794bff24a3d7a1757cc99da96c3b9
Stefan Samoylov <stefan28091996@gmail.com>
2023-07-08 13:46:46 +0500

    Вторая решена

--- Homework5/Work2/Program.cs
+++ Homework5/Work2/Program.cs
@@ -1,7 +1,13 @@
 ﻿int[] array = new int[4];
+int x = 0;
 Random rand = new Random();
 for (int i=0; i < array.Length; i++)
 {
-    array[i] = rand.Next(100, 1000);
+    array[i] = rand.Next(-99, 100);
+    if (i%2 == 1)
+    {
+        x = x + array[i];
+    }
 }
 System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine(x);
